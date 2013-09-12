2e9936403d295533cd9e8feb60ee9c33b5a9c8f9
Daniel Sanz <daniel.sanz@liferay.com>
2013-09-12 18:41:31 +0200

    code to create, build and run scripts

---
+++ symp-2013/ScriptBuilder.groovy
@@ -0,0 +1,40 @@
+import com.liferay.portal.kernel.cluster.ClusterExecutorUtil;
+import com.liferay.portal.kernel.scripting.ScriptingUtil;
+
+public class ScriptBuilder {
+	private String _code = "";
+	private String _baseUrl;
+
+	public ScriptBuilder(String baseURL) {
+		_baseUrl = baseURL;
+	}
+
+	public void append(String url) {
+		_code = _code + get(url);
+	}
+
+	public String get(String url) {
+		return new URL(_baseUrl + url).text
+	}
+
+	public String escape(String script) {
+		return script.replace("\"","\\\"").replace("\n","\\n");
+	}
+
+	public void replace(String text, String replacement) {
+		_code = _code.replace(text, replacement);
+	}
+
+	public String replace(String url, String text, String replacement) {
+		return get(url).replace(text, replacement);
+	}
+
+	public void echo() {
+		System.out.print(_code);
+	}
+
+	public void run() {
+		ScriptingUtil.clearCache("groovy");
+		ScriptingUtil.exec(null, null, "groovy", _code);
+	}
+}
