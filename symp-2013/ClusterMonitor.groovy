4203c5e0114de663d7dbf21ca020ecf0cdc8b16f
Daniel Sanz <daniel.sanz@liferay.com>
2013-09-13 12:41:37 +0200

    move master address computation to the cluster monitor

--- symp-2013/ClusterMonitor.groovy
+++ symp-2013/ClusterMonitor.groovy
@@ -1,9 +1,11 @@
 /* Cluster monitor prepares a set of ClusterMonitor Commands and execute them in cluster */
 
+String master = ClusterExecutorUtil.getLocalClusterNodeAddress().getRealAddress();
+
 sbCommand = new ScriptBuilder("https://raw.github.com/dsanz/scripts/master/symp-2013/");
 sbCommand.append("ScriptBuilder.groovy");
+sbCommand.appendCode("master=\""+  master + "\"");
 sbCommand.append("ClusterMonitorCommand.groovy");
-sbCommand.echo();
 sbCommand.runCluster();
 
 
--- symp-2013/ClusterMonitorCommand.groovy
+++ symp-2013/ClusterMonitorCommand.groovy
@@ -1,18 +1,5 @@
 import com.liferay.portal.kernel.cluster.ClusterExecutorUtil;
 
-/* new code */
 sb = new ScriptBuilder("https://raw.github.com/dsanz/scripts/master/symp-2013/");
-String master = ClusterExecutorUtil.getLocalClusterNodeAddress().getRealAddress();
 sb.appendCode(sb.replace("ResponseLogger.groovy", "@master", master));
-sb.echo();
 sb.runCluster();
-
-/* old code */
-/*
-sb = new ScriptBuilder("https://raw.github.com/dsanz/scripts/master/symp-2013/");
-sb.append("ClusterScriptRunner.groovy")
-String master = ClusterExecutorUtil.getLocalClusterNodeAddress().getRealAddress();
-String responseScript = sb.replace("ResponseLogger.groovy", "@master", master);
-sb.replace("@script", sb.escape(responseScript));
-sb.echo();
-sb.run();*/
