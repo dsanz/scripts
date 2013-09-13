ff97028c1fbd86d6e14205a46c13146cd4313a93
Daniel Sanz <daniel.sanz@liferay.com>
2013-09-13 08:37:19 +0200

    simplify cluster monitor to avoid copying the ScriptBuilder code inside it

--- symp-2013/ClusterMonitor.groovy
+++ symp-2013/ClusterMonitor.groovy
@@ -1,46 +1,9 @@
-public class ScriptBuilder {
-	private String _code = "";
-	private String _baseUrl;
+/* Cluster monitor prepares a set of ClusterMonitor Commands and execute them in cluster */
 
-	public ScriptBuilder(String baseURL) {
-		_baseUrl = baseURL;
-	}
+sbCommand = new ScriptBuilder("https://raw.github.com/dsanz/scripts/master/symp-2013/");
+sbCommand.append("ScriptBuilder.groovy");
+sbCommand.append("ClusterMonitorCommand.groovy");
+sbCommand.echo();
+sbCommand.runCluster();
 
-	public void append(String url) {
-		_code = _code + get(url);
-	}
 
-	public String get(String url) {
-		return new URL(_baseUrl + url).text
-	}
-
-	public String escape(String script) {
-		return script.replace("\"","\\\"").replace("\n","\\n");
-	}
-
-	public void replace(String text, String replacement) {
-		_code = _code.replace(text, replacement);
-	}
-
-	public String replace(String url, String text, String replacement) {
-		return get(url).replace(text, replacement);
-	}
-
-	public void echo() {
-		System.out.print(_code);
-	}
-
-	public void run() {
-		ScriptingUtil.clearCache("groovy");
-		ScriptingUtil.exec(null, null, "groovy", _code);
-	}
-}
-
-sb = new ScriptBuilder("https://raw.github.com/dsanz/scripts/master/symp-2013/");
-
-sb.append("ScriptBuilder.groovy")
-sb.append("ClusterScriptRunner.groovy")
-String requestScript = sb.get("ClusterMonitorCommand.groovy");
-sb.replace("@script", sb.escape(requestScript));
-sb.echo();
-sb.run();
