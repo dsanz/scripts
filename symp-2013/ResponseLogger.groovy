ccd4ce85b587e742d0f50c4dfca9f04a92a02391
Daniel Sanz <daniel.sanz@liferay.com>
2013-09-13 13:07:35 +0200

    make response logger independent of the code executed on each node

--- symp-2013/ResponseLogger.groovy
+++ symp-2013/ResponseLogger.groovy
@@ -2,18 +2,12 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.cluster.ClusterExecutorUtil;
 
-import java.lang.management.ManagementFactory ;
-import java.lang.management.MemoryPoolMXBean;
-import java.lang.management.RuntimeMXBean;
-
 String master = "@master";
+String node = "@node";
 String addr = ClusterExecutorUtil.getLocalClusterNodeAddress().getRealAddress();
 
 if (addr.equals(master)) {
-	Log _log = LogFactoryUtil.getLog(addr);
-	_log.error("Used Memory: " + ((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory())/ (1024 * 1024)) + " bytes");
-	_log.error("Free Memory: " + (Runtime.getRuntime().freeMemory() / (1024 * 1024)) + " bytes");
-	_log.error("Total Memory: " + (Runtime.getRuntime().totalMemory() / (1024 * 1024))+ " bytes");
-	_log.error("Max Memory: " + (Runtime.getRuntime().maxMemory() / (1024 * 1024))+ " bytes");
+	Log _log = LogFactoryUtil.getLog("From " + node);
+	_log.error("@log");
 }
 
