92fe441f69cbfdb608d9482ff744da226a3ffab1
Daniel Sanz <daniel.sanz@liferay.com>
2013-09-13 13:08:00 +0200

    make each cluster command able to reuse response logger

--- symp-2013/ClusterMonitorCommand.groovy
+++ symp-2013/ClusterMonitorCommand.groovy
@@ -1,5 +1,25 @@
 import com.liferay.portal.kernel.cluster.ClusterExecutorUtil;
 
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.cluster.ClusterExecutorUtil;
+
+import java.lang.management.ManagementFactory ;
+import java.lang.management.MemoryPoolMXBean;
+import java.lang.management.RuntimeMXBean;
+
+/* get local node address */
+String thisNode = ClusterExecutorUtil.getLocalClusterNodeAddress().getRealAddress();
+
+/* get monitoring info from this node */
+String used = "Used memory (" + thisNode + "): " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) + " bytes";
+
+/* prepare logger script with local data and run it */
 sb = new ScriptBuilder("https://raw.github.com/dsanz/scripts/master/symp-2013/");
-sb.appendCode(sb.replace("ResponseLogger.groovy", "@master", master));
+String responseLogger = sb.get("ResponseLogger.groovy");
+responseLogger = responseLogger.replace("@node", thisNode);
+responseLogger = responseLogger.replace("@master", master);
+responseLogger = responseLogger.replace("@log", used);
+sb.appendCode(responseLogger);
 sb.runCluster();
