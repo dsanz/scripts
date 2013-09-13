c40f79e7f847627cd51dfc349efe6fbc3d01bac3
Daniel Sanz <daniel.sanz@liferay.com>
2013-09-13 08:30:30 +0200

    try a new, sompler aproach to launch cluster exec w/o replacements

--- symp-2013/ClusterMonitorCommand.groovy
+++ symp-2013/ClusterMonitorCommand.groovy
@@ -1,9 +1,18 @@
 import com.liferay.portal.kernel.cluster.ClusterExecutorUtil;
 
+/* new code */
+sb = new ScriptBuilder("https://raw.github.com/dsanz/scripts/master/symp-2013/");
+String master = ClusterExecutorUtil.getLocalClusterNodeAddress().getRealAddress();
+sb.append(sb.replace("ResponseLogger.groovy", "@master", master));
+sb.echo();
+sb.runCluster();
+
+/* old code */
+/*
 sb = new ScriptBuilder("https://raw.github.com/dsanz/scripts/master/symp-2013/");
 sb.append("ClusterScriptRunner.groovy")
 String master = ClusterExecutorUtil.getLocalClusterNodeAddress().getRealAddress();
 String responseScript = sb.replace("ResponseLogger.groovy", "@master", master);
 sb.replace("@script", sb.escape(responseScript));
 sb.echo();
-sb.run();
+sb.run();*/
