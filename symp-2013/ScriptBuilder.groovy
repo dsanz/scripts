b1cab1cff0d17fec4e2f6b5b9071acd7cde950a7
Daniel Sanz <daniel.sanz@liferay.com>
2013-09-13 08:46:11 +0200

    add missing imports

--- symp-2013/ScriptBuilder.groovy
+++ symp-2013/ScriptBuilder.groovy
@@ -1,3 +1,11 @@
+import com.liferay.portal.kernel.scheduler.Trigger;
+import com.liferay.portal.kernel.scheduler.StorageType;
+import com.liferay.portal.kernel.scheduler.SchedulerEngineUtil;
+import com.liferay.portal.kernel.scheduler.SchedulerEngine;
+import com.liferay.portal.kernel.messaging.DestinationNames;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.scheduler.IntervalTrigger;
+import com.liferay.portal.kernel.cluster.ClusterExecutorUtil;
 import com.liferay.portal.kernel.cluster.ClusterExecutorUtil;
 import com.liferay.portal.kernel.scripting.ScriptingUtil;
 
