b13616509cd25ba5f0fc4576a55dd696c605d147
Daniel Sanz <daniel.sanz@liferay.com>
2013-09-16 14:16:22 +0200

    Add ability to report the command result to the cluster

--- symp-2013/CommandResultWriter.groovy
+++ symp-2013/CommandResultWriter.groovy
@@ -1,5 +1,10 @@
+import com.liferay.portal.kernel.cache.MultiVMPoolUtil
+import com.liferay.portal.kernel.cache.PortalCache
 import com.liferay.portal.kernel.json.JSONFactoryUtil
 import com.liferay.portal.kernel.json.JSONObject
+import com.liferay.portal.kernel.cache.MultiVMPoolUtil;
+import com.liferay.portal.kernel.cache.PortalCache;
+
 
 public class CommandResultWriter {
 	private String _who;
@@ -17,5 +22,12 @@ public class CommandResultWriter {
 
 	public String getResult() {
 		return result.toString();
+	}
+
+	public void done() {
+		ScriptBuilder sb = new ScriptBuilder("");
+		sb.appendCode("PortalCache pc = MultiVMPoolUtil.getCache(\"CLUSTER_MONITOR\");")
+		sb.appendCode("pc.put(" + _who + ", " + getResult() + ");");
+		sb.runCluster();
 	}
 }
