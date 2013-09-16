dc22302d2fc384b5fa5eab6127d292da2e9a4d99
Daniel Sanz <daniel.sanz@liferay.com>
2013-09-16 16:28:59 +0200

    fix syntax error

--- symp-2013/ClusterMonitorMemoryUsageCommand.groovy
+++ symp-2013/ClusterMonitorMemoryUsageCommand.groovy
@@ -1,11 +1,9 @@
-new ClusterCommand(){
+new ClusterCommand() {
 	public void execute() {
 		/* get monitoring info from this node */
 		addResult("used", String.valueOf(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()));
 		addResult("total", String.valueOf(Runtime.getRuntime().totalMemory()));
 		addResult("free", String.valueOf(Runtime.getRuntime().freeMemory()));
 	}
-}).run();
-
-
+}.run();
 
