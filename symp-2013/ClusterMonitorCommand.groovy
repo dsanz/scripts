752447acd891d8050f4e4ee1cf01ec7f65d7641e
Daniel Sanz <daniel.sanz@liferay.com>
2013-09-16 09:14:41 +0200

    add log message

--- symp-2013/ClusterMonitorCommand.groovy
+++ symp-2013/ClusterMonitorCommand.groovy
@@ -1,21 +1,18 @@
 import com.liferay.portal.kernel.cache.MultiVMPoolUtil
 import com.liferay.portal.kernel.cache.PortalCache
-import com.liferay.portal.kernel.cluster.ClusterExecutorUtil;
-
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.cluster.ClusterExecutorUtil;
-
-import java.lang.management.ManagementFactory ;
-import java.lang.management.MemoryPoolMXBean;
-import java.lang.management.RuntimeMXBean;
 
 /* get local node address */
 String thisNode = ClusterExecutorUtil.getLocalClusterNodeAddress().getRealAddress();
 
 /* get monitoring info from this node */
 Long used = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+
+Log _log = LogFactoryUtil.getLog("local");
+_log.error("Running cluster monitor command")
 
 PortalCache pc = MultiVMPoolUtil.getCache("CLUSTER_MONITOR");
 
