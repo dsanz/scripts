0738d92823334b78a0d8ad8c14d142b1dc8e76c6
Daniel Sanz <daniel.sanz@liferay.com>
2013-09-16 09:41:03 +0200

    minor refactorings

--- symp-2013/ClusterMonitor.groovy
+++ symp-2013/ClusterMonitor.groovy
@@ -5,46 +5,54 @@ import com.liferay.portal.kernel.cache.PortalCache
 import com.liferay.portal.kernel.log.Log
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.io.Serializable;
+import com.liferay.portal.kernel.cache.CacheListener;
+
 PortalCache pc = MultiVMPoolUtil.getCache("CLUSTER_MONITOR");
-
-
 
 String master = ClusterExecutorUtil.getLocalClusterNodeAddress().getRealAddress();
 
-pc.registerCacheListener(new com.liferay.portal.kernel.cache.CacheListener(){
+public class ClusterMonitorCacheListener implements CacheListener {
 	long _putsCount=0;
-	long _expectedPuts=ClusterExecutorUtil.getClusterNodeAddresses().size();
-	Log _log = LogFactoryUtil.getLog("test");
+	long _expectedPuts;
+	Log _log;;
+	List<Serializable> keys;
 
-	List<Serializable> keys = new ArrayList<Serializable>();
+	public ClusterMonitorCacheListener(int clusterSize) {
+		_log = LogFactoryUtil.getLog("test")
+		keys = new ArrayList<Serializable>();
+		_log.error("Creating ClusterMonitorCacheListener, size: " + clusterSize)
+		_expectedPuts = clusterSize;
+	}
 
-		public void notifyEntryEvicted(
+	public void notifyEntryEvicted(
 			PortalCache portalCache, Serializable key, Object value) {}
-		public void notifyEntryExpired(
+	public void notifyEntryExpired(
 			PortalCache portalCache, Serializable key, Object value) {}
 
-		public void notifyEntryPut(
+	public void notifyEntryPut(
 			PortalCache portalCache, Serializable key, Object value) {
-		 	_putsCount++;
-			_log.error("np " + _putsCount);
-			keys.add(key);
-			if (putsCount == _expectedPuts) {
-				// process all data and create some aggregated data
-				PortalCache pcache = MultiVMPoolUtil.getCache("CLUSTER_MONITOR");
-				for (String k : keys) {
-					_log.error("Recv data: " + k + " -> " + (Long)(pcache.get(k)));
-				}
-				// output it
+		_putsCount++;
+		_log.error("np " + _putsCount);
+		keys.add(key);
+		if (putsCount == _expectedPuts) {
+			// process all data and create some aggregated data
+			for (String k : keys) {
+				_log.error("Recv data: " + k + " -> " + (Long)(portalCache.get(k)));
 			}
+			// output it
 		}
-		public void notifyEntryRemoved(
+	}
+	public void notifyEntryRemoved(
 			PortalCache portalCache, Serializable key, Object value) {}
-		public void notifyEntryUpdated(
+	public void notifyEntryUpdated(
 			PortalCache portalCache, Serializable key, Object value) {}
-		public void notifyRemoveAll(PortalCache portalCache) {}
+	public void notifyRemoveAll(PortalCache portalCache) {}
+}
 
-});
-
+pc.registerCacheListener(new ClusterMonitorCacheListener(ClusterExecutorUtil.getClusterNodeAddresses().size));
 sbCommand = new ScriptBuilder("https://raw.github.com/dsanz/scripts/cache/symp-2013/");
 sbCommand.append("ScriptBuilder.groovy");
 sbCommand.appendCode("master=\""+  master + "\"");
