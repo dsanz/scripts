83c7bfaeebfcda3a9b69dc52d5bf2c4418118138
Daniel Sanz <daniel.sanz@liferay.com>
2013-09-16 14:26:40 +0200

    add missing import

--- symp-2013/ClusterMonitorCommand.groovy
+++ symp-2013/ClusterMonitorCommand.groovy
@@ -1,5 +1,7 @@
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.cluster.ClusterExecutorUtil;
+
 
 /* get local node address */
 String thisNode = ClusterExecutorUtil.getLocalClusterNodeAddress().getRealAddress();
