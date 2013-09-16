ef5c413824b1da8f7e6718c0b2526c3deeacdb0a
Daniel Sanz <daniel.sanz@liferay.com>
2013-09-16 08:31:10 +0200

    let cluster monitor command to use distributed cache to store the command results

--- symp-2013/ClusterMonitorCommand.groovy
+++ symp-2013/ClusterMonitorCommand.groovy
@@ -1,3 +1,5 @@
+import com.liferay.portal.kernel.cache.MultiVMPoolUtil
+import com.liferay.portal.kernel.cache.PortalCache
 import com.liferay.portal.kernel.cluster.ClusterExecutorUtil;
 
 
@@ -13,13 +15,9 @@ import java.lang.management.RuntimeMXBean;
 String thisNode = ClusterExecutorUtil.getLocalClusterNodeAddress().getRealAddress();
 
 /* get monitoring info from this node */
-String used = "Used memory (" + thisNode + "): " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) + " bytes";
+Long used = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
 
-/* prepare logger script with local data and run it */
-sb = new ScriptBuilder("https://raw.github.com/dsanz/scripts/master/symp-2013/");
-String responseLogger = sb.get("ResponseLogger.groovy");
-responseLogger = responseLogger.replace("@node", thisNode);
-responseLogger = responseLogger.replace("@master", master);
-responseLogger = responseLogger.replace("@log", used);
-sb.appendCode(responseLogger);
-sb.runCluster();
+PortalCache pc = MultiVMPoolUtil.getCache("CLUSTER_MONITOR");
+
+pc.put(thisNode, used);
+
