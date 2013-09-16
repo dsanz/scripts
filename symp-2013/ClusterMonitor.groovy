237f57b2b03d7ce7c987a4b2543b857aed01a6ee
Daniel Sanz <daniel.sanz@liferay.com>
2013-09-16 18:28:44 +0200

    use our own div for result

--- symp-2013/ClusterMonitor.groovy
+++ symp-2013/ClusterMonitor.groovy
@@ -36,10 +36,11 @@ while (!cl.isDone()) {
 	Thread.sleep(50);
 }
 out.print("done!<br>");
+out.print("<div id='clustermonitorresult'></div>");
 out.println("<script src='https://raw.github.com/padolsey/prettyPrint.js/master/prettyprint.js'></script>");
 out.println("<script type='text/javascript'>");
 out.println("var r='" + cl.getResultAsString() + "';");
 out.println("var tbl = prettyPrint(r);");
-out.println("document.body.appendChild(tbl);");
+out.println("document.getElementById('clustermonitorresult').appendChild(tbl);");
 out.println("</script>");
 
