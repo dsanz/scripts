eb4e8145f4937b9c8a4818dc5b39e266cbbeaadf
Daniel Sanz <daniel.sanz@liferay.com>
2013-09-17 13:07:29 +0200

    sf

--- symp-2013/CommandResultWriter.groovy
+++ symp-2013/CommandResultWriter.groovy
@@ -3,23 +3,25 @@ import com.liferay.portal.kernel.json.JSONFactoryUtil
 import com.liferay.portal.kernel.json.JSONObject
 
 public class CommandResultWriter {
-	private String _who;
-	JSONObject result;
-	JSONObject payload;
+	private String _cacheKey;
+	private JSONObject _result;
+	private JSONObject _payload;
 
-	public CommandResultWriter(String key) {
-		_who = ClusterExecutorUtil.getLocalClusterNodeAddress().getRealAddress() + "!" + key;
-		result = JSONFactoryUtil.createJSONObject();
-		payload = JSONFactoryUtil.createJSONObject();
-		result.put(key, payload);
+	public CommandResultWriter(String command) {
+		String thisNode = ClusterExecutorUtil.getLocalClusterNodeAddress().getRealAddress();
+		_cacheKey = thisNode + "!" + command;
+		System.out.println("Cacke KEY: " + _cacheKey);
+		_result = JSONFactoryUtil.createJSONObject();
+		_payload = JSONFactoryUtil.createJSONObject();
+		_result.put(command, _payload);
 	}
 
 	public addResult(String key, String value) {
-		payload.put(key, value);
+		_payload.put(key, value);
 	}
 
 	public String getResult() {
-		return result.toString();
+		return _result.toString();
 	}
 
 	public void done() {
@@ -27,10 +29,10 @@ public class CommandResultWriter {
 		sb.appendCode("import com.liferay.portal.kernel.log.LogFactoryUtil;");
 		sb.appendCode("import com.liferay.portal.kernel.cache.PortalCache");
 		sb.appendCode("import com.liferay.portal.kernel.cache.MultiVMPoolUtil")
-		sb.appendCode("_log = LogFactoryUtil.getLog(\"CommandResultWriter_" + _who + "\");");
+		sb.appendCode("_log = LogFactoryUtil.getLog(\"ResultWriter_" + _cacheKey + "\");");
 		sb.appendCode("_log.error(\"Running command result writer\");");
 		sb.appendCode("PortalCache pc = MultiVMPoolUtil.getCache(\"CLUSTER_MONITOR\");")
-		sb.appendCode("pc.put(\"" + _who + "\", \"" + getResult().replace("\"", "\\\"") + "\");");
+		sb.appendCode("pc.put(\"" + _cacheKey + "\", \"" + getResult().replace("\"", "\\\"") + "\");");
 		sb.runCluster();
 	}
 }
