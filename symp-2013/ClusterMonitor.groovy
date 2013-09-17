2a527a4c7ce5121c1ce2cb2dd899e8ddf143c13d
Daniel Sanz <daniel.sanz@liferay.com>
2013-09-17 09:02:25 +0200

    fix typo

--- symp-2013/ClusterMonitor.groovy
+++ symp-2013/ClusterMonitor.groovy
@@ -30,7 +30,7 @@ sbCommand.appendCode("master=\""+  master + "\"");
 sbCommand.append("CommandResultWriter.groovy");
 sbCommand.append("ClusterCommand.groovy");
 sbCommand.append("ClusterMonitorMemoryUsageCommand.groovy");
-sbCommand.append("ClusterGetPortalImplsManifestAttrsCommand.groovy");
+sbCommand.append("ClusterGetPortalImplManifestAttrsCommand.groovy");
 sbCommand.runCluster();
 
 while (!cl.isDone()) {
