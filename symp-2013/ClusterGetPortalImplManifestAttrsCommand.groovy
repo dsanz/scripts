008cbbf58b53b995dbdc1cf81ceaeda76349c95d
Daniel Sanz <daniel.sanz@liferay.com>
2013-09-17 08:28:10 +0200

    new command that gets portal-impl manifest attrs

---
+++ symp-2013/ClusterGetPortalImplManifestAttrsCommand.groovy
@@ -0,0 +1,31 @@
+import java.util.jar.Attributes
+import de.schlichtherle.io.File;
+import de.schlichtherle.io.FileInputStream;
+import java.util.jar.Manifest
+
+public class ClusterGetPortalImplManifestAttrsCommand extends ClusterCommand {
+	public String PORTAL_LIB_NAME = "portal-impl";
+	public String PORTAL_LIB_PATH =  PORTAL_LIB_NAME + ".jar";
+
+	public void execute() {
+		File portalImplManifest = new File(PortalUtil.getPortalLibDir() +
+			PORTAL_LIB_PATH + "/META-INF/MANIFEST.MF");
+
+		FileInputStream fis = null;
+
+		try {
+			fis = new FileInputStream(portalImplManifest);
+			Attributes attributes = new Manifest(fis).getMainAttributes();
+			for (String attrName : attributes.keySet()) {
+				addResult(attrName, attributes.getValue(attrName));
+			}
+		}
+		finally {
+			if (fis != null) {
+				fis.close();
+			}
+		}
+	}
+}
+
+new ClusterGetPortalImplManifestAttrsCommand().run();
