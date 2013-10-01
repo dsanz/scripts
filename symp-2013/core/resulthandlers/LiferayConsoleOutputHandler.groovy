b67cee40189350c382f501e5c95f81a38deb5db7
Daniel Sanz <daniel.sanz@liferay.com>
2013-10-01 12:33:32 +0200

    Add a ResulHandler able to generate a pretty printed json representation of the commands result and outputting it to the script console out writer

---
+++ symp-2013/core/resulthandlers/LiferayConsoleOutputHandler.groovy
@@ -0,0 +1,20 @@
+import com.liferay.portal.kernel.io.unsync.UnsyncPrintWriter;
+
+public class LiferayConsoleOutputHandler implements ResultHandler {
+	UnsyncPrintWriter _out;
+
+	public LiferayConsoleOutputHandler(UnsyncPrintWriter out) {
+		_out = out;
+	}
+
+	public void done(CommandResultListener crl) {
+		_out.print("done!<br>");
+		_out.print("<div id='clustermonitorresult'></div>");
+		_out.println("<script src='https://raw.github.com/padolsey/prettyPrint.js/master/prettyprint.js'></script>");
+		_out.println("<script type='text/javascript'>");
+		_out.println("var r=" + crl.getResultAsString() + ";");
+		_out.println("var tbl = prettyPrint(r);");
+		_out.println("document.getElementById('clustermonitorresult').appendChild(tbl);");
+		_out.println("</script>");
+	}
+}
