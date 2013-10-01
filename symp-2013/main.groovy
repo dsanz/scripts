d495c054fc2bb24972fbd841bfc13de2fbe39db1
Daniel Sanz <daniel.sanz@liferay.com>
2013-10-01 17:39:16 +0200

    add frameworkloader to the mix

--- symp-2013/main.groovy
+++ symp-2013/main.groovy
@@ -1,6 +1,7 @@
 import com.liferay.portal.kernel.scripting.ScriptingUtil;
 String baseURL = "https://raw.github.com/dsanz/scripts/master/symp-2013/"
 
+String framework = new URL(baseURL + "util/FrameworkLoader.groovy").text
 String launcher = new URL(baseURL + "util/launcher.groovy").text
 
 launcher.replace("@", "util/ClusterMonitorSample.groovy")
@@ -8,4 +9,4 @@ launcher.replace("@", "util/ClusterMonitorSample.groovy")
 Map<String, Object> env = new HashMap<String, Object>();
 env.put("out", out);
 
-ScriptingUtil.exec(null, env, "groovy", launcher);
+ScriptingUtil.exec(null, env, "groovy", framework + "\n" + launcher);
