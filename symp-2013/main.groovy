bea03fa327747911afbe4ea0999d2cdb4b2929a2
Daniel Sanz <daniel.sanz@liferay.com>
2013-10-01 17:35:59 +0200

    let main be the one who uses the launcher, indicating the code which act as logic and running all

--- symp-2013/main.groovy
+++ symp-2013/main.groovy
@@ -1,7 +1,6 @@
 import com.liferay.portal.kernel.scripting.ScriptingUtil;
 String baseURL = "https://raw.github.com/dsanz/scripts/master/symp-2013/"
 
-String frameworkLoader = new URL(baseURL + "util/FrameworkLoader.groovy").text
 String launcher = new URL(baseURL + "util/launcher.groovy").text
 
 launcher.replace("@", "util/ClusterMonitorSample.groovy")
@@ -9,4 +8,4 @@ launcher.replace("@", "util/ClusterMonitorSample.groovy")
 Map<String, Object> env = new HashMap<String, Object>();
 env.put("out", out);
 
-ScriptingUtil.exec(null, env, "groovy", frameworkLoader + "\n" + launcher);
+ScriptingUtil.exec(null, env, "groovy", launcher);
