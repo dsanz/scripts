35aa4fd6e795c20c1ae79bca1ad3b99a04626fc8
Daniel Sanz <daniel.sanz@liferay.com>
2013-10-01 12:34:12 +0200

    Simplify ClusterMonitor to make use of the ResultHandlers in combination with commandBuilder

--- symp-2013/util/ClusterMonitor.groovy
+++ symp-2013/util/ClusterMonitor.groovy
@@ -2,20 +2,11 @@
 
 out.print("Starting...");
 
-sbCommand = new CommandBuilder("https://raw.github.com/dsanz/scripts/cache/symp-2013/", true);
-sbCommand.appendCommand("MemoryUsageCommand.groovy", "MemoryUsageCommand");
-sbCommand.appendCommand("GetPortalImplManifestAttrsCommand.groovy", "GetPortalImplManifestAttrsCommand");
-sbCommand.start();
+boolean isCluster=true;
 
-while (!cl.isDone()) {
-	Thread.sleep(50);
-}
-out.print("done!<br>");
-out.print("<div id='clustermonitorresult'></div>");
-out.println("<script src='https://raw.github.com/padolsey/prettyPrint.js/master/prettyprint.js'></script>");
-out.println("<script type='text/javascript'>");
-out.println("var r=" + cl.getResultAsString() + ";");
-out.println("var tbl = prettyPrint(r);");
-out.println("document.getElementById('clustermonitorresult').appendChild(tbl);");
-out.println("</script>");
+sbCommand = new CommandBuilder("https://raw.github.com/dsanz/scripts/cache/symp-2013/", isCluster);
+sbCommand.appendCommand("commands/MemoryUsageCommand.groovy", "MemoryUsageCommand");
+sbCommand.appendCommand("commands/GetPortalImplManifestAttrsCommand.groovy", "GetPortalImplManifestAttrsCommand");
+sbCommand.start(new LiferayConsoleOutputHandler(out));
+
 
