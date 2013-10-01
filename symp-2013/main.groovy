8e782c5ec2be8b9c6617e4f8b8c0d05c279eabe8
Daniel Sanz <daniel.sanz@liferay.com>
2013-10-01 17:21:39 +0200

    make main responsible of composing the whole script (framework + launcher) and run it

--- symp-2013/main.groovy
+++ symp-2013/main.groovy
@@ -2,9 +2,11 @@ import com.liferay.portal.kernel.scripting.ScriptingUtil;
 String baseURL = "https://raw.github.com/dsanz/scripts/master/symp-2013/"
 
 String frameworkLoader = new URL(baseURL + "util/FrameworkLoader.groovy").text
-String clusterMonitor = new URL(baseURL + "util/ClusterMonitorSample.groovy").text
+String launcher = new URL(baseURL + "util/launcher.groovy").text
+
+launcher.replace("@", "util/ClusterMonitorSample.groovy")
 
 Map<String, Object> env = new HashMap<String, Object>();
 env.put("out", out);
 
-ScriptingUtil.exec(null, env, "groovy", frameworkLoader + "\n" +  clusterMonitor);
+ScriptingUtil.exec(null, env, "groovy", frameworkLoader + "\n" + launcher);
