856a2b30e501e9720203fc61e2aea692a2ccd576
Daniel Sanz <daniel.sanz@liferay.com>
2013-10-01 17:58:32 +0200

    set base url to the right value

--- symp-2013/util/ClusterMonitorSample.groovy
+++ symp-2013/util/ClusterMonitorSample.groovy
@@ -4,9 +4,9 @@ out.print("Starting...");
 
 boolean isCluster=true;
 
-cb = new CommandBuilder("https://raw.github.com/dsanz/scripts/master/symp-2013/commands/", isCluster);
-cb.appendCommand("MemoryUsageCommand.groovy", "MemoryUsage");
-cb.appendCommand("GetPortalImplManifestAttrsCommand.groovy", "ManifestAttrs");
+cb = new CommandBuilder("https://raw.github.com/dsanz/scripts/master/symp-2013/", isCluster);
+cb.appendCommand("commands/MemoryUsageCommand.groovy", "MemoryUsage");
+cb.appendCommand("commands/GetPortalImplManifestAttrsCommand.groovy", "ManifestAttrs");
 cb.start(new LiferayConsoleOutputHandler(out));
 
 
