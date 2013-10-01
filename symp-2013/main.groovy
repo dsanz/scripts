2ff7f5831df3e2da2a97a49a56ce5adb59a24f84
Daniel Sanz <daniel.sanz@liferay.com>
2013-10-01 17:50:29 +0200

    fix wrong replacement

--- symp-2013/main.groovy
+++ symp-2013/main.groovy
@@ -4,7 +4,7 @@ String baseURL = "https://raw.github.com/dsanz/scripts/master/symp-2013/"
 String framework = new URL(baseURL + "util/FrameworkLoader.groovy").text
 String launcher = new URL(baseURL + "util/launcher.groovy").text
 
-launcher.replace("@", "util/ClusterMonitorSample.groovy")
+launcher = launcher.replace("@", "util/ClusterMonitorSample.groovy")
 
 Map<String, Object> env = new HashMap<String, Object>();
 env.put("out", out);
