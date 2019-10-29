6cc65c872cd5ca18a03d660126ec85ded6addbec
Daniel Sanz <daniel.sanz@liferay.com>
2019-10-29 17:11:46 +0100

    Use unix newlines as os.EOL (portable option) makes license parsing to break

--- activation-key-merger/akm.js
+++ activation-key-merger/akm.js
@@ -1,21 +1,20 @@
 var fs = require('fs'),
-	glob = require("glob"),
-	os = require("os");
+	glob = require("glob");
 
 function writeLicense(filename, fromFiles) {
 	let fd;
 	try {
 		fd = fs.openSync(filename, 'w');
-		fs.appendFileSync(fd, '<?xml version="1.0"?>' + os.EOL, 'utf8');
-		fs.appendFileSync(fd, '<licenses>' + os.EOL, 'utf8');
+		fs.appendFileSync(fd, '<?xml version="1.0"?>' + '\n', 'utf8');
+		fs.appendFileSync(fd, '<licenses>' + '\n', 'utf8');
 		for (let index = 0; index < fromFiles.length; index++) {
 			var lines = fs.readFileSync(fromFiles[index], 'utf-8').split('\n');
 			// note we skip first and second lines
 			for (let lx = 2; lx < lines.length; lx++) {
-				fs.appendFileSync(fd, "\t" + lines[lx] + os.EOL, 'utf8');
+				fs.appendFileSync(fd, '\t' + lines[lx] + '\n', 'utf8');
 			}
 		}
-		fs.appendFileSync(fd, '</licenses>' + os.EOL, 'utf8');
+		fs.appendFileSync(fd, '</licenses>' + '\n', 'utf8');
 	}
 	catch (err) {
 		console.log(err);
